84125caca7febc89049c012c6c39bbb3f1d45fd2
Fayyaz Alam <fayazalam.98@gmail.com>
2022-04-22 10:01:42 +0500

    array data

---
+++ aray.js
@@ -0,0 +1,255 @@
+[[
+    [
+        "Activity",
+        [
+            [
+                "RockClimbing",
+                [
+                    "trad Climbing",
+                    "Big Walling",
+                    "Sport Climbing"
+                ]
+            ],
+            [
+                "Canyoing",
+                [
+                    "Wet Canyons",
+                    "Dry Canyons"
+                ]
+            ],
+            [
+                "hiliting"
+            ],
+            [
+                "RopeJumping"
+            ],
+            [
+                "Caving"
+            ]
+        ]
+    ],
+    [
+        "GEAR",
+        [
+            [
+                "Ropes",
+                [
+                    "Dynamic Ropes",
+                    "Static Ropes"
+                ]
+            ],
+            [
+                "Belay Device",
+                [
+                    "tubular",
+                    "Assisted Breaking"
+                ]
+            ],
+            [
+                "CARABINERS",
+                [
+                    "Wiregate",
+                    "Locking",
+                    "Non-Locking"
+                ]
+            ],
+            [
+                "Cams"
+            ],
+            [
+                "Nuts"
+            ],
+            [
+                "Bolts"
+            ]
+        ]
+    ],
+    [
+        "Episode Flavor",
+        [
+            [
+                "Stunts",
+                [
+                    "Sketchy",
+                    "Well Planned"
+                ]
+            ]
+        ]
+    ]
+
+]]
+
+
+//new array
+[
+    [
+        "ACTIVITY",
+        [
+            [
+                "Rock Climbing",
+                [
+                    "Trad Climbing",
+                    "Big Walling",
+                    "Sport Climbing"
+                ]
+            ],
+            [
+                "Canyoing",
+                [
+                    "Wet Canyons",
+                    "Dry Canyons"
+                ]
+            ],
+            [
+                "Highlining"
+            ],
+            [
+                "RopeJumping"
+            ],
+            [
+                "Caving"
+            ]
+        ]
+    ],
+    [
+        "GEAR",
+        [
+            [
+                "Ropes",
+                [
+                    "Dynamic Ropes",
+                    "Static Ropes"
+                ]
+            ],
+            [
+                "Belay Devices",
+                [
+                    "Tubular",
+                    "Assisted Breaking"
+                ]
+            ],
+            [
+                "Carabiners",
+                [
+                    "Wiregate",
+                    "Locking",
+                    "Non-Locking"
+                ]
+            ],
+            [
+                "Cams",
+
+            ],
+            [
+                "Nuts",
+
+            ],
+            [
+                "Bolts",
+
+            ],
+            [
+                "Miscellaneous",
+
+            ]
+        ]
+    ],
+    [
+        "EPISODE FLAVOR",
+        [
+            [
+                "Stunts",
+                [
+                    "Sketchy",
+                    "Well Planned"
+                ]
+            ],
+            [
+                "Science",
+                [
+                    "Dynamic Testing",
+                    "Bolt Busters",
+                    "Slow Pull",
+                    "Human testing"
+                ]
+            ],
+            [
+                "Buying Guides",
+                [
+                    "Big Picture",
+                    "Specific Genre",
+                    "Specific Product"
+                ],
+            ],
+            [
+                "Monday Updates"
+
+            ],
+            [
+                "How (Not) 2 - Instructionals",
+                [
+                    "Breaking Test Machines",
+                    "Load Cells"
+                ]
+            ],
+            [
+                "Purely For Fun",
+
+            ]
+
+
+        ]
+    ],
+    [
+        "ROCK TYPE",
+        [
+            [
+                "Granite",
+            ],
+            [
+                "Basalt",
+            ],
+            [
+                "Limestone",
+            ],
+            [
+                "Sandstone",
+            ]
+        ]
+    ],
+    [
+        "MATERIAL",
+        [
+            [
+                "Steel",
+            ],
+            [
+                "Aluminum",
+            ],
+            [
+                "Titanium",
+            ],
+            [
+                "Dyneema",
+            ],
+            [
+                "Nylon",
+            ]
+
+        ]
+    ],
+    [
+        "LOCATION",
+        [
+            [
+                "Lab",
+            ],
+            [
+                "Outdoors",
+            ],
+            [
+                "Gym",
+            ]
+        ]
+    ]
+]
